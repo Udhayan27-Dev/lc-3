73587dc04c6e812cff57671016460b533f130c02
Udhayan K <kudhayan27@gmail.com>
2026-06-30 23:47:16 +0530

    feat: impl AND opcode for the lc-3 vm

--- src/hardware/instruction/mod.rs
+++ src/hardware/instruction/mod.rs
@@ -26,7 +26,7 @@ pub fn exec_instr(instr:u16,vm:&mut VM){
 
     match op_code {
         Some(Opcode::ADD) => add(instr, vm),
-        // Some(Opcode::AND) => and(),
+        Some(Opcode::AND) => and(instr,vm),
         // Some(Opcode::NOT) => not(),
         // Some(Opcode::BR) => br(),
         // Some(Opcode::JMP) => jmp(),
@@ -108,6 +108,33 @@ pub fn ldi(instr:u16,vm:&mut VM){
     vm.registers.update(dr, res);
     vm.registers.update_cond(dr);
 }
+
+/// "AND" opcode
+/// 15           12 │11        9│8         6│ 5 │4     3│2         0
+/// ┌───────────────┼───────────┼───────────┼───┼───────┼───────────┐
+/// │      0101     │     DR    │  SR1      │ 0 │  00   │    SR2    │
+/// └───────────────┴───────────┴───────────┴───┴───────┴───────────┘
+
+///  15           12│11        9│8         6│ 5 │4                 0
+/// ┌───────────────┼───────────┼───────────┼───┼───────────────────┐
+/// │      0101     │     DR    │  SR1      │ 1 │       IMM5        │
+/// └───────────────┴───────────┴───────────┴───┴───────────────────┘
+
+pub fn and(instr: u16,vm: &mut VM){
+    let dr = (instr >> 9) & 0x7;
+    let sr1 = (instr >> 6) & 0x7;
+
+    let imm_flag = (instr >> 5) & 0x1;
+    if imm_flag == 1 {
+        let imm5 = sign_extend(instr & 0x1F, 5);
+        vm.registers.update(dr, vm.registers.get(sr1) & imm5);
+    }
+    else {
+        let sr2 = instr & 0x7;
+        vm.registers.update(dr, vm.registers.get(sr1) & vm.registers.get(sr2));
+    }
+    vm.registers.update_cond(dr);
+
 
 pub fn sign_extend(mut x:u16,bit_count:u8) -> u16 {
     if(x >> (bit_count-1)) & 1 != 0 {
